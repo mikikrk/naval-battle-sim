ce211ac32e2143e0da4944a5987b4f1eb5f52038
Kamil Kopczyk <kamil.kopczyk@bluepaprica.com>
2013-12-01 19:01:33 +0100

    Init commit

---
+++ src/navalbattlesim/NavalBattleSim.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package navalbattlesim;
+
+/**
+ *
+ * @author bober
+ */
+public class NavalBattleSim {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
